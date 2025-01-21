c72a5ce741e3a40713c2602639b0e900e04eb5b6
Rafael Machado <rafaelmachadojeziorny@gmail.com>
2025-01-21 17:25:32 -0300

    Correção commit passado

--- src/main/java/com/rafaelmachado/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/rafaelmachado/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -1,17 +1,15 @@
 package com.rafaelmachado.workshopmongo.resources.exception;
 
-import com.rafaelmachado.workshopmongo.services.exception.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import com.rafaelmachado.workshopmongo.services.exception.ObjectNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
-
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
--- src/main/java/com/rafaelmachado/workshopmongo/services/UserService.java
+++ src/main/java/com/rafaelmachado/workshopmongo/services/UserService.java
@@ -5,7 +5,7 @@ import com.rafaelmachado.workshopmongo.repository.UserRepository;
 import com.rafaelmachado.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.Optional;
 import java.util.List;
 
 @Service
@@ -18,11 +18,8 @@ public class UserService {
         return repo.findAll();
     }
 
-    public User findById(String id){
-        User user = repo.findById(id).get();
-        if (user == null){
-            throw new ObjectNotFoundException("Objeto não encontrado");
-        }
-        return user;
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
