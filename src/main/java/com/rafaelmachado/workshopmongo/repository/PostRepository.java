80d4cff81e85ba4445a288458694d6df9c017af0
Rafael Machado <rafaelmachadojeziorny@gmail.com>
2025-01-22 16:28:17 -0300

    Consulta simples com query methods

--- src/main/java/com/rafaelmachado/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rafaelmachado/workshopmongo/repository/PostRepository.java
@@ -2,10 +2,12 @@ package com.rafaelmachado.workshopmongo.repository;
 
 import com.rafaelmachado.workshopmongo.domain.Post;
 
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
