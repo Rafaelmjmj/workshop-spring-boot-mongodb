561a6ef83279b706c3721aed0a36e6eab286e132
Rafael Machado <rafaelmachadojeziorny@gmail.com>
2025-01-22 16:46:55 -0300

    Consulta com varios criterios

--- src/main/java/com/rafaelmachado/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rafaelmachado/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.rafaelmachado.workshopmongo.repository;
 
 import com.rafaelmachado.workshopmongo.domain.Post;
 
+import java.util.Date;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -14,4 +15,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> searchTitle(String title);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, " +
+            "{ 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/rafaelmachado/workshopmongo/resources/PostResource.java
+++ src/main/java/com/rafaelmachado/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.rafaelmachado.workshopmongo.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
+import java.util.Date;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,19 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+
+    @RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue="") String text,
+            @RequestParam(value="minDate", defaultValue="") String minDate,
+            @RequestParam(value="maxDate", defaultValue="") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
